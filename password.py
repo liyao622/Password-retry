97d8c87f257d43e1693590bd98c78c584fd99823
Leo Wu <leo0622@hotmail.com.tw>
2019-08-02 01:07:45 +0800

    Password retry v3

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = 'a123456'
 x = 3
-while x <= 3 and x >= 1:
+while x > 0:
+	x = x-1
 	pwd = input('請輸入密碼: ')
 	if pwd == 'a123456':
 		print('登入成功')
 		break
 	else:
-		x = x-1
-		print('密碼錯誤! 還有', x, '次機會')
-		if x == 0:
-			print('此帳號已鎖定')
-			break
+		print('密碼錯誤! ')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('帳號已鎖住')
